6e0a056575d4ae4a84923ed197b13acad0edd64b
Emma Hack <ehack78@gmail.com>
2020-10-30 17:18:26 +0000

    sixth test play_game__scissors_vs_paper passed

--- app/models/game.py
+++ app/models/game.py
@@ -9,4 +9,12 @@ class Game:
             return player1
         elif player1.choice == "scissors" and player2.choice == "rock":
             return player2
+        elif player1.choice == "rock" and player2.choice == "scissors":
+            return player1
+        elif player1.choice == "rock" and player2.choice == "paper":
+            return player2
+        elif player1.choice == "paper" and player2.choice == "scissors":
+            return player2
+        elif player1.choice == "paper" and player2.choice == "rock":
+            return player1
 
--- app/models/game_test.py
+++ app/models/game_test.py
@@ -7,6 +7,7 @@ class TestGame(unittest.TestCase):
     def setUp(self):
         self.bob = Player("Bob", "scissors")
         self.alice = Player("Alice", "rock")
+        self.janet = Player("Janet", "paper")
         self.game = Game(self.bob, self.alice)
 
     def test_game_has_a_player__player1(self):
@@ -18,4 +19,8 @@ class TestGame(unittest.TestCase):
     def test_play_game__scissors_vs_rock(self):
         self.game.play_game(self.bob, self.alice)
         self.assertEqual(self.alice, self.game.play_game(self.bob, self.alice))
+
+    def test_play_game__scissors_vs_paper(self):
+        self.game.play_game(self.bob, self.janet)
+        self.assertEqual(self.bob, self.game.play_game(self.bob, self.janet))
 
